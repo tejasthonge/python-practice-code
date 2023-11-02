648d0680075a1e3c16d8868329a20f304b5380cf
Amar_raj <amarthonge6767@gmail.com>
2023-11-02 14:01:07 +0530

    return void

---
+++ 4Function/1noReturnVal/c0.py
@@ -0,0 +1,14 @@
+##Functions
+##      it similar as the method or functions of other language
+##      but in python it have multiple return values 
+
+
+
+##########################
+def fun(a,b):   
+    print(a+b)
+
+x = int(input("x :"));
+y = int(input("y :"));
+
+fun(x,y)  #call to fun 
