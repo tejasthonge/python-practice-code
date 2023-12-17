af2ee0da6e000c316703a3250f73a88bc0a51da6
Amar_raj <amarthonge6767@gmail.com>
2023-12-17 15:55:21 +0530

    5 rem

---
+++ 3ControlStatment/0ASSIGNMENT/5/c1.py
@@ -0,0 +1,20 @@
+
+
+
+'''
+Nested for Patterns
+
+Program1 :
+
+Rows = 3
+* * *
+* * *
+* * *
+'''
+
+for i in range(0,3):
+
+    for j in range(0,3):
+        print( "*" ,end=" ")
+
+    print()
---
+++ 3ControlStatment/0ASSIGNMENT/5/c3.py
@@ -0,0 +1,19 @@
+
+
+'''
+Program3 :
+Rows = 4
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+
+'''
+
+row = 4 
+
+
+for j in range(0,row):
+    for i in range (1,row+1):
+        print(i,end=" ")
+    print()
