b2174ed1652a21609cede15cc39a6d11e99aa5fa
Amar_raj <amarthonge6767@gmail.com>
2024-01-07 15:52:15 +0530

    dir

---
+++ 23Multiple-Inheritance/1MRO/c0.py
@@ -0,0 +1,59 @@
+
+
+#In python how achive multiple inheritnce :
+
+
+#python2 :
+'''
+    -it mh work differt algorithms so its outputs ordar changes 
+    -by defoalt object class is not parent 
+    -if we have make the oure class object parent then we have write in code 
+    -if we cheak the type of any class it will print <class 'classobj'>
+    -means it will rewrite in python3 
+'''
+
+#python3:
+
+'''
+--in 23 we rearn python3 by defoalt its letss verion in 23
+--in python3 python achive mejor change thire version
+--it rewrite thir class 
+--by defoalt all class have paret object class meand it inherits all of tingd from parent object class
+--we dont have to write object class () inside the brackete
+--if we print type of our class it print <class 'type'>
+'''
+
+
+class Demo: 
+    pass
+
+
+#run on both python2 and python2 and check the output
+
+print(dir(Demo))  #contante of class inhrerited or own
+print()
+print(type(Demo)) #print wich type of our class
+
+#OP:
+
+
+#for puthon3: 
+'''
+python3 c3.py
+
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
+
+<class 'type'>
+
+
+
+'''
+
+#for python2
+'''
+
+
+'''
+
+
+
